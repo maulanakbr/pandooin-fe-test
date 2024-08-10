5ac353efe5615a7ee7aeef7ec52a178733401b25
Maulana Akbar Yudistika <yudistika.akbar@gmail.com>
2024-08-11 00:21:23 +0700

    feat(main): set metadata

---
+++ src/lib/site-config.ts
@@ -0,0 +1,5 @@
+export const siteConfig: Record<string, string> = {
+  title: 'Zamrood by Pandooin',
+  description: 'Premium Travel Experiences in Indonesia',
+  url: 'https://pandooin-fe-test-khaki.vercel.app/',
+};
